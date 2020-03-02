73e0af007a7e5adef67096735b0ac67ad7b68078
machine <janwa039@gmail.com>
2020-03-02 18:22:47 +0000

    322

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To install a particular build branch or beta branch with the Github URL, run:
 ### In the browser :
 ```html
 <script src="https://unpkg.com/@kandy-io/callme-sdk@4.14.0/dist/kandy.js"
-  integrity="sha384-7g8yacVwauxxT443CW6m8yvHNqpZkdcZeNnPLNBDf+lXNysXRx8QlyG5KLwnqIRI"
+  integrity="sha384-97kysy3lkXuidhxOmfzSNcjIg09lTFKDg0CW52yPafCZp0IQHmsWMnq5z1aSBNB8"
   crossorigin="anonymous">
 </script>
 ```
--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.newCallMe.js
- * Version: 4.14.0-beta.321
+ * Version: 4.14.0-beta.322
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -41218,7 +41218,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '4.14.0-beta.321';
+  return '4.14.0-beta.322';
 }
 
 /***/ }),
